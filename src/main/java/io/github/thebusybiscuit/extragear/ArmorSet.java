cbaa6e8b85bd8fb5aee58f87acd6740079230bfd
TheBusyBiscuit <mrCookieSlime@gmail.com>
2020-04-17 13:22:09 +0200

    Updated to latest Slimefun build

--- src/main/java/io/github/thebusybiscuit/extragear/ArmorSet.java
+++ src/main/java/io/github/thebusybiscuit/extragear/ArmorSet.java
@@ -3,38 +3,38 @@ package io.github.thebusybiscuit.extragear;
 import org.bukkit.Material;
 
 enum ArmorSet {
-	
-	LEATHER(Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS),
-	IRON(Material.IRON_HELMET, Material.IRON_CHESTPLATE, Material.IRON_LEGGINGS, Material.IRON_BOOTS),
-	GOLD(Material.GOLDEN_HELMET, Material.GOLDEN_CHESTPLATE, Material.GOLDEN_LEGGINGS, Material.GOLDEN_BOOTS),
-	DIAMOND(Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS);
-	
-	private final Material helmet;
-	private final Material chestplate;
-	private final Material leggings;
-	private final Material boots;
-	
-	ArmorSet(Material helmet, Material chestplate, Material leggings, Material boots) {
-		this.helmet = helmet;
-		this.chestplate = chestplate;
-		this.leggings = leggings;
-		this.boots = boots;
-	}
-	
-	public Material getHelmet() {
-		return helmet;
-	}
-	
-	public Material getChestplate() {
-		return chestplate;
-	}
-	
-	public Material getLeggings() {
-		return leggings;
-	}
-	
-	public Material getBoots() {
-		return boots;
-	}
+
+    LEATHER(Material.LEATHER_HELMET, Material.LEATHER_CHESTPLATE, Material.LEATHER_LEGGINGS, Material.LEATHER_BOOTS), 
+    IRON(Material.IRON_HELMET, Material.IRON_CHESTPLATE, Material.IRON_LEGGINGS, Material.IRON_BOOTS), 
+    GOLD(Material.GOLDEN_HELMET, Material.GOLDEN_CHESTPLATE, Material.GOLDEN_LEGGINGS, Material.GOLDEN_BOOTS), 
+    DIAMOND(Material.DIAMOND_HELMET, Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS);
+
+    private final Material helmet;
+    private final Material chestplate;
+    private final Material leggings;
+    private final Material boots;
+
+    ArmorSet(Material helmet, Material chestplate, Material leggings, Material boots) {
+        this.helmet = helmet;
+        this.chestplate = chestplate;
+        this.leggings = leggings;
+        this.boots = boots;
+    }
+
+    public Material getHelmet() {
+        return helmet;
+    }
+
+    public Material getChestplate() {
+        return chestplate;
+    }
+
+    public Material getLeggings() {
+        return leggings;
+    }
+
+    public Material getBoots() {
+        return boots;
+    }
 
 }
--- src/main/java/io/github/thebusybiscuit/extragear/ExtraGear.java
+++ src/main/java/io/github/thebusybiscuit/extragear/ExtraGear.java
@@ -10,14 +10,12 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
-import me.mrCookieSlime.CSCoreLibPlugin.general.String.StringUtils;
-import me.mrCookieSlime.Slimefun.Lists.Categories;
+import io.github.thebusybiscuit.slimefun4.utils.ChatUtils;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.Lists.SlimefunItems;
+import me.mrCookieSlime.Slimefun.Objects.Category;
 import me.mrCookieSlime.Slimefun.Objects.Research;
 import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
-import me.mrCookieSlime.Slimefun.Setup.SlimefunManager;
-import me.mrCookieSlime.Slimefun.api.Slimefun;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 import me.mrCookieSlime.Slimefun.bstats.bukkit.Metrics;
 import me.mrCookieSlime.Slimefun.cscorelib2.collections.Pair;
@@ -27,204 +25,108 @@ import me.mrCookieSlime.Slimefun.cscorelib2.updater.GitHubBuildsUpdater;
 
 public class ExtraGear extends JavaPlugin implements SlimefunAddon {
 
-	private int researchId = 3300;
+    private int researchId = 3300;
+    private Category category;
 
-	@Override
-	public void onEnable() {
-		Config cfg = new Config(this);
-		
-		if (cfg.getBoolean("options.auto-update")) {
-			new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/ExtraGear/master").start();
-		}
-		
-		new Metrics(this, 6469);
+    @Override
+    public void onEnable() {
+        Config cfg = new Config(this);
 
-		registerSword(Material.IRON_SWORD, "COPPER", SlimefunItems.COPPER_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_UNDEAD, 2)
-		));
-		
-		registerArmor(ArmorSet.LEATHER, "COPPER", SlimefunItems.COPPER_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 2)
-		));
+        if (cfg.getBoolean("options.auto-update")) {
+            new GitHubBuildsUpdater(this, getFile(), "TheBusyBiscuit/ExtraGear/master").start();
+        }
 
-		registerSword(Material.IRON_SWORD, "TIN", SlimefunItems.TIN_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 1)
-		));
-		
-		registerArmor(ArmorSet.IRON, "TIN", SlimefunItems.TIN_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 3)
-		));
+        new Metrics(this, 6469);
 
-		registerSword(Material.IRON_SWORD, "SILVER", SlimefunItems.SILVER_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 2)
-		));
-		
-		registerArmor(ArmorSet.IRON, "SILVER", SlimefunItems.SILVER_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 2)
-		));
+        category = new Category(new NamespacedKey(this, "items"), new CustomItem(Material.DIAMOND_SWORD, "&6ExtraGear"));
 
-		registerSword(Material.IRON_SWORD, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 3)
-		));
-		
-		registerArmor(ArmorSet.IRON, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 2),
-			new Pair<>(Enchantment.DURABILITY, 2)
-		));
+        registerSword(Material.IRON_SWORD, "COPPER", SlimefunItems.COPPER_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_UNDEAD, 2)));
+        registerArmor(ArmorSet.LEATHER, "COPPER", SlimefunItems.COPPER_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 2)));
 
-		registerSword(Material.IRON_SWORD, "LEAD", SlimefunItems.LEAD_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 3),
-			new Pair<>(Enchantment.DURABILITY, 8)
-		));
-		
-		registerArmor(ArmorSet.IRON, "LEAD", SlimefunItems.LEAD_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3),
-			new Pair<>(Enchantment.DURABILITY, 8)
-		));
+        registerSword(Material.IRON_SWORD, "TIN", SlimefunItems.TIN_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 1)));
+        registerArmor(ArmorSet.IRON, "TIN", SlimefunItems.TIN_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 3)));
 
-		registerSword(Material.IRON_SWORD, "ZINC", SlimefunItems.ZINC_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 2)
-		));
-		
-		registerArmor(ArmorSet.IRON, "ZINC", SlimefunItems.ZINC_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3)
-		));
+        registerSword(Material.IRON_SWORD, "SILVER", SlimefunItems.SILVER_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 2)));
+        registerArmor(ArmorSet.IRON, "SILVER", SlimefunItems.SILVER_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 2)));
 
-		registerSword(Material.IRON_SWORD, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 2),
-			new Pair<>(Enchantment.DURABILITY, 5)
-		));
-		
-		registerArmor(ArmorSet.IRON, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 2),
-			new Pair<>(Enchantment.DURABILITY, 5)
-		));
+        registerSword(Material.IRON_SWORD, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 3)));
+        registerArmor(ArmorSet.IRON, "ALUMINUM", SlimefunItems.ALUMINUM_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_EXPLOSIONS, 2), new Pair<>(Enchantment.DURABILITY, 2)));
 
-		registerSword(Material.IRON_SWORD, "STEEL", SlimefunItems.STEEL_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 5),
-			new Pair<>(Enchantment.DURABILITY, 6)
-		));
-		
-		registerArmor(ArmorSet.IRON, "STEEL", SlimefunItems.STEEL_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3),
-			new Pair<>(Enchantment.DURABILITY, 4)
-		));
+        registerSword(Material.IRON_SWORD, "LEAD", SlimefunItems.LEAD_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 3), new Pair<>(Enchantment.DURABILITY, 8)));
+        registerArmor(ArmorSet.IRON, "LEAD", SlimefunItems.LEAD_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3), new Pair<>(Enchantment.DURABILITY, 8)));
 
-		registerSword(Material.IRON_SWORD, "BRONZE", SlimefunItems.BRONZE_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 3),
-			new Pair<>(Enchantment.DURABILITY, 6)
-		));
-		
-		registerSword(Material.IRON_SWORD, "DURALUMIN", SlimefunItems.DURALUMIN_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 3),
-			new Pair<>(Enchantment.DURABILITY, 6)
-		));
-		
-		registerSword(Material.IRON_SWORD, "BILLON", SlimefunItems.BILLON_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 4),
-			new Pair<>(Enchantment.DURABILITY, 5)
-		));
-		
-		registerSword(Material.IRON_SWORD, "BRASS", SlimefunItems.BRASS_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_UNDEAD, 4),
-			new Pair<>(Enchantment.DURABILITY, 6)
-		));
-		
-		registerSword(Material.IRON_SWORD, "ALUMINUM_BRASS", SlimefunItems.ALUMINUM_BRASS_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 4),
-			new Pair<>(Enchantment.DURABILITY, 4)
-		));
-		
-		registerSword(Material.IRON_SWORD, "ALUMINUM_BRONZE", SlimefunItems.ALUMINUM_BRONZE_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 4),
-			new Pair<>(Enchantment.DURABILITY, 5)
-		));
-		
-		registerSword(Material.IRON_SWORD, "CORINTHIAN_BRONZE", SlimefunItems.CORINTHIAN_BRONZE_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 5),
-			new Pair<>(Enchantment.DURABILITY, 5)
-		));
-		
-		registerSword(Material.IRON_SWORD, "SOLDER", SlimefunItems.SOLDER_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 4),
-			new Pair<>(Enchantment.DURABILITY, 6)
-		));
-		
-		registerSword(Material.IRON_SWORD, "DAMASCUS_STEEL", SlimefunItems.DAMASCUS_STEEL_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 6),
-			new Pair<>(Enchantment.DURABILITY, 7)
-		));
-		
-		registerSword(Material.IRON_SWORD, "HARDENED", SlimefunItems.HARDENED_METAL_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 7),
-			new Pair<>(Enchantment.DURABILITY, 10)
-		));
-		
-		registerSword(Material.IRON_SWORD, "REINFORCED", SlimefunItems.REINFORCED_ALLOY_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 8),
-			new Pair<>(Enchantment.DURABILITY, 8)
-		));
-		
-		registerSword(Material.IRON_SWORD, "FERROSILICON", SlimefunItems.FERROSILICON, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_UNDEAD, 8),
-			new Pair<>(Enchantment.DURABILITY, 4)
-		));
-		
-		registerSword(Material.GOLDEN_SWORD, "GILDED_IRON", SlimefunItems.GILDED_IRON, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 8),
-			new Pair<>(Enchantment.DURABILITY, 10)
-		));
-		
-		registerSword(Material.IRON_SWORD, "NICKEL", SlimefunItems.NICKEL_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 6),
-			new Pair<>(Enchantment.DURABILITY, 5)
-		));
+        registerSword(Material.IRON_SWORD, "ZINC", SlimefunItems.ZINC_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 2)));
+        registerArmor(ArmorSet.IRON, "ZINC", SlimefunItems.ZINC_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3)));
 
-		registerSword(Material.IRON_SWORD, "COBALT", SlimefunItems.COBALT_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.DAMAGE_ALL, 7),
-			new Pair<>(Enchantment.DURABILITY, 7)
-		));
-		
-		registerArmor(ArmorSet.IRON, "COBALT", SlimefunItems.COBALT_INGOT, Arrays.asList(
-			new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 7),
-			new Pair<>(Enchantment.DURABILITY, 7)
-		));
-	}
+        registerSword(Material.IRON_SWORD, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 2), new Pair<>(Enchantment.DURABILITY, 5)));
+        registerArmor(ArmorSet.IRON, "MAGNESIUM", SlimefunItems.MAGNESIUM_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 2), new Pair<>(Enchantment.DURABILITY, 5)));
 
-	private void registerSword(Material type, String component, ItemStack item, List<Pair<Enchantment, Integer>> enchantments) {
-		SlimefunItemStack is = new SlimefunItemStack(component + "_SWORD", type, "&r" + StringUtils.format(component) + " Sword");
+        registerSword(Material.IRON_SWORD, "STEEL", SlimefunItems.STEEL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 5), new Pair<>(Enchantment.DURABILITY, 6)));
+        registerArmor(ArmorSet.IRON, "STEEL", SlimefunItems.STEEL_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 3), new Pair<>(Enchantment.DURABILITY, 4)));
 
-		for (Pair<Enchantment, Integer> enchantment : enchantments) {
-			is.addUnsafeEnchantment(enchantment.getFirstValue(), enchantment.getSecondValue());
-		}
-		
-		new SlimefunItem(Categories.WEAPONS, is, RecipeType.ENHANCED_CRAFTING_TABLE,
-		new ItemStack[] {null, item, null, null, item, null, null, new ItemStack(Material.STICK), null})
-		.register(this);
-		
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, component.toLowerCase() + "_sword"), researchId, StringUtils.format(component) + " Sword", 3), is);
-	}
+        registerSword(Material.IRON_SWORD, "BRONZE", SlimefunItems.BRONZE_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 3), new Pair<>(Enchantment.DURABILITY, 6)));
+        registerSword(Material.IRON_SWORD, "DURALUMIN", SlimefunItems.DURALUMIN_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 3), new Pair<>(Enchantment.DURABILITY, 6)));
+        registerSword(Material.IRON_SWORD, "BILLON", SlimefunItems.BILLON_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 4), new Pair<>(Enchantment.DURABILITY, 5)));
+        registerSword(Material.IRON_SWORD, "BRASS", SlimefunItems.BRASS_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_UNDEAD, 4), new Pair<>(Enchantment.DURABILITY, 6)));
+        registerSword(Material.IRON_SWORD, "ALUMINUM_BRASS", SlimefunItems.ALUMINUM_BRASS_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 4), new Pair<>(Enchantment.DURABILITY, 4)));
+        registerSword(Material.IRON_SWORD, "ALUMINUM_BRONZE", SlimefunItems.ALUMINUM_BRONZE_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 4), new Pair<>(Enchantment.DURABILITY, 5)));
+        registerSword(Material.IRON_SWORD, "CORINTHIAN_BRONZE", SlimefunItems.CORINTHIAN_BRONZE_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 5), new Pair<>(Enchantment.DURABILITY, 5)));
+        registerSword(Material.IRON_SWORD, "SOLDER", SlimefunItems.SOLDER_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 4), new Pair<>(Enchantment.DURABILITY, 6)));
+        registerSword(Material.IRON_SWORD, "DAMASCUS_STEEL", SlimefunItems.DAMASCUS_STEEL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 6), new Pair<>(Enchantment.DURABILITY, 7)));
+        registerSword(Material.IRON_SWORD, "HARDENED", SlimefunItems.HARDENED_METAL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 7), new Pair<>(Enchantment.DURABILITY, 10)));
+        registerSword(Material.IRON_SWORD, "REINFORCED", SlimefunItems.REINFORCED_ALLOY_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 8), new Pair<>(Enchantment.DURABILITY, 8)));
+        registerSword(Material.IRON_SWORD, "FERROSILICON", SlimefunItems.FERROSILICON, Arrays.asList(new Pair<>(Enchantment.DAMAGE_UNDEAD, 8), new Pair<>(Enchantment.DURABILITY, 4)));
+        registerSword(Material.GOLDEN_SWORD, "GILDED_IRON", SlimefunItems.GILDED_IRON, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ARTHROPODS, 8), new Pair<>(Enchantment.DURABILITY, 10)));
+        registerSword(Material.IRON_SWORD, "NICKEL", SlimefunItems.NICKEL_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 6), new Pair<>(Enchantment.DURABILITY, 5)));
 
-	private void registerArmor(ArmorSet armorset, String component, ItemStack item, List<Pair<Enchantment, Integer>> enchantments) {
-		ItemStack[] armor = {
-			new CustomItem(armorset.getHelmet(), "&r" + StringUtils.format(component) + " Helmet"),
-			new CustomItem(armorset.getChestplate(), "&r" + StringUtils.format(component) + " Chestplate"),
-			new CustomItem(armorset.getLeggings(), "&r" + StringUtils.format(component) + " Leggings"),
-			new CustomItem(armorset.getBoots(), "&r" + StringUtils.format(component) + " Boots")
-		};
-		
-		for (Pair<Enchantment, Integer> enchantment : enchantments) {
-			for (ItemStack is : armor) {
-				is.addUnsafeEnchantment(enchantment.getFirstValue(), enchantment.getSecondValue());
-			}
-		}
-		
-		SlimefunManager.registerArmorSet(item, armor, component, false, this);
+        registerSword(Material.IRON_SWORD, "COBALT", SlimefunItems.COBALT_INGOT, Arrays.asList(new Pair<>(Enchantment.DAMAGE_ALL, 7), new Pair<>(Enchantment.DURABILITY, 7)));
+        registerArmor(ArmorSet.IRON, "COBALT", SlimefunItems.COBALT_INGOT, Arrays.asList(new Pair<>(Enchantment.PROTECTION_ENVIRONMENTAL, 7), new Pair<>(Enchantment.DURABILITY, 7)));
+    }
 
-		researchId++;
-		Slimefun.registerResearch(new Research(new NamespacedKey(this, component.toLowerCase() + "_armor"), researchId, StringUtils.format(component) + " Armor", 5), armor);
-	}
+    private void registerSword(Material type, String component, ItemStack item, List<Pair<Enchantment, Integer>> enchantments) {
+        SlimefunItemStack is = new SlimefunItemStack(component + "_SWORD", type, "&r" + ChatUtils.humanize(component) + " Sword");
+
+        for (Pair<Enchantment, Integer> enchantment : enchantments) {
+            is.addUnsafeEnchantment(enchantment.getFirstValue(), enchantment.getSecondValue());
+        }
+
+        SlimefunItem slimefunItem = new SlimefunItem(category, is, RecipeType.ENHANCED_CRAFTING_TABLE, new ItemStack[] { null, item, null, null, item, null, null, new ItemStack(Material.STICK), null });
+        slimefunItem.register(this);
+
+        researchId++;
+
+        Research research = new Research(new NamespacedKey(this, component.toLowerCase() + "_sword"), researchId, ChatUtils.humanize(component) + " Sword", 3);
+        research.addItems(slimefunItem);
+        research.register();
+    }
+
+    private void registerArmor(ArmorSet armorset, String component, ItemStack item, List<Pair<Enchantment, Integer>> enchantments) {
+        SlimefunItemStack[] armor = { new SlimefunItemStack(component + "_HELMET", armorset.getHelmet(), "&r" + ChatUtils.humanize(component) + " Helmet"), new SlimefunItemStack(component + "_CHESTPLATE", armorset.getChestplate(), "&r" + ChatUtils.humanize(component) + " Chestplate"), new SlimefunItemStack(component + "_LEGGINGS", armorset.getLeggings(), "&r" + ChatUtils.humanize(component) + " Leggings"), new SlimefunItemStack(component + "_BOOTS", armorset.getBoots(), "&r" + ChatUtils.humanize(component) + " Boots") };
+
+        for (Pair<Enchantment, Integer> enchantment : enchantments) {
+            for (ItemStack is : armor) {
+                is.addUnsafeEnchantment(enchantment.getFirstValue(), enchantment.getSecondValue());
+            }
+        }
+
+        SlimefunItem helmet = new SlimefunItem(category, armor[0], RecipeType.ARMOR_FORGE, new ItemStack[] { item, item, item, item, null, item, null, null, null });
+        helmet.register(this);
+
+        SlimefunItem chestplate = new SlimefunItem(category, armor[1], RecipeType.ARMOR_FORGE, new ItemStack[] { item, null, item, item, item, item, item, item, item });
+        chestplate.register(this);
+
+        SlimefunItem leggings = new SlimefunItem(category, armor[2], RecipeType.ARMOR_FORGE, new ItemStack[] { item, item, item, item, null, item, item, null, item });
+        leggings.register(this);
+
+        SlimefunItem boots = new SlimefunItem(category, armor[3], RecipeType.ARMOR_FORGE, new ItemStack[] { null, null, null, item, null, item, item, null, item });
+        boots.register(this);
+
+        researchId++;
+
+        Research research = new Research(new NamespacedKey(this, component.toLowerCase() + "_armor"), researchId, ChatUtils.humanize(component) + " Armor", 5);
+        research.addItems(helmet, chestplate, leggings, boots);
+        research.register();
+    }
 
     @Override
     public JavaPlugin getJavaPlugin() {
